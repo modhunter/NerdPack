9035a4a9aeeb47bce5ee6e56945dac44352e79a4
Jaime Marques <mrthesoulz@gmail.com>
2016-10-07 03:16:29 +0100

    OM stuff

--- system/om.lua
+++ system/om.lua
@@ -2,22 +2,32 @@ local _, NeP = ...
 
 NeP.OM = {}
 
-local OM = {
+local OM_c = {
 	Enemies = {},
 	Friendly = {},
 	Dead = {},
 	Objects = {}
 }
 
+function NeP.OM:Garbage()
+	for tb in pairs(OM_c) do
+		for _, obj in pairs(OM_c[tb]) do
+			if not UnitExists(obj.key) then
+				obj = nil
+			end
+		end 
+	end
+end
+
 function NeP.OM:Get(ref)
-	return OM[ref]
+	return OM_c[ref]
 end
 
 function NeP.OM:Filter(ref, GUID)
-	if not OM[ref][GUID] then return end
-	local key = OM[ref][GUID].key
+	if not OM_c[ref][GUID] then return end
+	local key = OM_c[ref][GUID].key
 	local distance = NeP.Protected:Distance('player', Obj)
-	OM[ref][GUID].distance = distance
+	OM_c[ref][GUID].distance = distance
 	return true
 end
 
@@ -26,9 +36,9 @@ function NeP.OM:Insert(ref, Obj)
 	if self:Filter(ref, Obj) then return end
 	-- Add it
 	local GUID = UnitGUID(Obj) or '0'
-	local objectType, _, _, _, _, ObjID, _ = strsplit('-', GUID)
+	local ObjID = select(6, strsplit('-', GUID))
 	local distance = NeP.Protected:Distance('player', Obj)
-	OM[ref][GUID] = {
+	OM_c[ref][GUID] = {
 		key = Obj,
 		name = UnitName(Obj),
 		distance = distance,
@@ -40,25 +50,26 @@ end
 function NeP.OM:Add(Obj)
 	-- Dead Units
 	if UnitIsDeadOrGhost(Obj) then
-		self:Insert('Enemies', Obj)
+		NeP.OM:Insert('Enemies', Obj)
 	-- Friendly
 	elseif UnitIsFriend('player', Obj) then
-		self:Insert('Friendly', Obj)
+		NeP.OM:Insert('Friendly', Obj)
 	-- Enemie
 	elseif UnitCanAttack('player', Obj) then
-		self:Insert('Dead', Obj)
+		NeP.OM:Insert('Dead', Obj)
 	-- Object
 	elseif ObjectWithIndex and ObjectIsType(Obj, ObjectTypes.GameObject) then
-		self:Insert('Objects', Obj)
+		NeP.OM:Insert('Objects', Obj)
 	end
 end
 
 C_Timer.NewTicker(0.1, (function()
-	
+	NeP.OM:Garbage()
 end), nil)
 
 -- Gobals
 NeP.Globals.OM = {
 	Add = NeP.OM.Add,
-	Get = NeP.OM.Get
+	Get = NeP.OM.Get,
+	test = NeP.OM.Garbage
 }
